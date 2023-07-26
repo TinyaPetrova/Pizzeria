4e595c61085ace63c10d1519452131aaf32fcca7
Ustinya Petrova <ust.petrova@gmail.com>
2023-07-26 21:32:26 +0200

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+
+  // MVP - minimum viable product - минимально жизнеспособный продукт
+  // Продукт, который можно использовать для демонстрации идеи
+
+  // Написать небольшую программу, используемую для обработки заказов в пиццерии
+
+  // Команды:
+  // - начать заказ
+  //   - указать данные о пицце: (на любом этапе можно вернуться к предыдущему этапу / отменить заказ)
+  //      - название
+  //      - размер (выбор из списка)
+  //      - автоматический расчёт стоимости
+  //   - данные о заказе добавляются в файл (подаются на кухню) (заказ завершён)
+  // - выход
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    while (true) {
+      MenuCommand command = MenuCommand.readCommand(scanner);
+      switch (command) {
+        case START:
+          Pizza pizza = Pizza.readData(scanner);
+          break;
+        case UNEXPECTED:
+          System.out.println("Некорректная команда");
+        case EXIT:
+          return; // завершение работы метода main()
+      }
+    }
+  }
+}
---
+++ src/MenuCommand.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public enum MenuCommand {
+  UNEXPECTED(""), // служебное значение, которого не должно быть (его не должен видеть юзер)
+  START("Начать заказ"),
+  EXIT("Выйти");
+
+  private final String message;
+
+  MenuCommand(String message) {
+    this.message = message;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  public static MenuCommand readCommand(Scanner scanner) {
+    printMenu();
+    System.out.print("Введите команду: ");
+    if (!scanner.hasNext()) {
+      throw new RuntimeException("Ожидается ввод команды");
+    }
+    String input = scanner.next();
+    scanner.nextLine();
+    switch (input.toLowerCase()) {
+      case "1": // Integer.toString(START.ordinal())
+      case "start":
+      case "начать":
+        return START;
+      case "2": // Integer.toString(EXIT.ordinal())
+      case "exit":
+      case "выйти":
+      case "выход":
+        return EXIT;
+      default:
+        return UNEXPECTED;
+    }
+  }
+
+  public static void printMenu() {
+    for (MenuCommand command : values()) {
+      if (!command.message.isEmpty()) {
+        System.out.println(command.ordinal() + ". " + command.message);
+      }
+    }
+  }
+}
+
+
+
